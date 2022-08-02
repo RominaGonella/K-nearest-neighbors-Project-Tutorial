847d4c54a732316ae906e85c7fea604ac0200f2e
RominaGonella <rominagonfur@gmail.com>
2022-08-02 23:01:22 +0000

    versión final

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Template for Machine Learning projects
+# Resumen del proceso
+
+1. El objetivo de la tarea consiste en construir un sistema de recomendación de películas sencillo utilizando KNN.
+2. Se utilizan dos datasets: uno de películas y otro de créditos de dichas películas, ambos provienen de  TMDB Movie Database API y contienen aproximadamente 5000 películas cada uno (2803). Contienen información sobre la película, como el título, género, palabras clave, director, etc. Para este trabajo se seleccionan solamente algunas de las variables.
+3. Se realiza un procesamiento de los datos de interés, eliminando filas con missing values, extrayendo solamente la información deseada de los campos de tipo json, quitando espacios entre palabras, etc.
+4. Se crea una variable que concatena las 5 variables a utilizar: overview, genres, keywords, cast y crew. Sobre esta variable se aplica la vectorización y luego se calcula el cosine_similarity, que mide la similaridad entre las distintas películas (de acuerdo a todas las variables en simultáneo).
+5. Se crea función de recomendaciones utilizando el resultado del cosine similarity, para cada input (película) devuelve las 5 películas más similares a ella.
+6. Se prueba el sistema con algunas películas elegidas al azar, y para la famosa película Titanic.
--- src/app.py
+++ src/app.py
@@ -1,4 +1,99 @@
-from utils import db_connect
-engine = db_connect()
+# K-nearest neighbors Project Tutorial
 
-# your code here
+# librerías
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import ast
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# cargo datos del repositorio
+movies = pd.read_csv('data/raw/tmdb_5000_movies.csv')
+credits = pd.read_csv('data/raw/tmdb_5000_credits.csv')
+
+# se unen ambos datasets
+movies = movies.merge(credits, on = 'title')
+
+# sólo variables de interés
+movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
+
+# se eliminan 3 filas con missing values
+movies.dropna(inplace = True)
+
+# función para traer el contenido de name de variable "genre"
+def convert(obj):
+    L = []
+    for i in ast.literal_eval(obj):
+        L.append(i['name'])
+    return L
+
+# se aplica función a "genres" y a "keywords"
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+
+# función para traer los 3 primeros elementos de name en una columna
+def convert3(obj):
+    L = []
+    count = 0
+    for i in ast.literal_eval(obj):
+        if count < 3:
+            L.append(i['name'])
+        count +=1  
+    return L
+
+# aplico función a "cast"
+movies['cast'] = movies['cast'].apply(convert3)
+
+# función para traer el nombre del director
+def fetch_director(obj):
+    L = []
+    for i in ast.literal_eval(obj):
+        if i['job'] == 'Director':
+            L.append(i['name'])
+            break
+    return L
+
+# se aplica función
+movies['crew'] = movies['crew'].apply(fetch_director)
+
+# se convierte columna overview a una lista
+movies['overview'] = movies['overview'].apply(lambda x : x.split())
+
+# función para quitar espacios entre palabras
+def collapse(L):
+    L1 = []
+    for i in L:
+        L1.append(i.replace(" ",""))
+    return L1
+
+# se aplica función a varias columnas
+movies['cast'] = movies['cast'].apply(collapse)
+movies['crew'] = movies['crew'].apply(collapse)
+movies['genres'] = movies['genres'].apply(collapse)
+movies['keywords'] = movies['keywords'].apply(collapse)
+
+# se crea nueva variable con información de 5 previas
+movies['tags'] = movies['overview']+movies['genres']+movies['keywords']+movies['cast']+movies['crew']
+
+# se crea nuevo dataset con 3 variables
+new_df = movies[['movie_id','title','tags']]
+new_df['tags'] = new_df['tags'].apply(lambda x :" ".join(x))
+
+# se crea vectorizador para un máximo de 5000 palabras
+cv = CountVectorizer(max_features=5000 ,stop_words='english')
+vectors = cv.fit_transform(new_df['tags']).toarray()
+
+# se calcula similaridad entre películas
+cosine_similarity(vectors).shape
+similarity = cosine_similarity(vectors)
+
+# función que identifica las 5 películas más similares a movie (sin contarla a ella misma)
+def recommend(movie):
+    movie_index = new_df[new_df['title'] == movie].index[0] ##fetching the movie index
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate( distances)),reverse =True , key = lambda x:x[1])[1:6]
+    
+    for i in movie_list:
+        print(new_df.iloc[i[0]].title)
